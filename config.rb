9974724787d128d811ed66731b597f77683c9891
Han Loong Liauw <hanloongliauw@gmail.com>
2015-11-30 08:17:12 +1100

    update again

--- config.rb
+++ config.rb
@@ -102,7 +102,7 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
-  deploy.remote = 'https://github.com/breudrinks/breudrinks.git'
+  deploy.remote = 'https://github.com/breudrinks/breudrinks.github.io.git'
 end
 
 helpers do
